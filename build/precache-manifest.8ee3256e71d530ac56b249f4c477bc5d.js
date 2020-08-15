cc8ccaf800d43e01232f1027c109423d899bbe08
Marten <marten.frisk@gmail.com>
2020-08-15 23:53:31 +0200

    Yarn upgrade

---
+++ build/precache-manifest.8ee3256e71d530ac56b249f4c477bc5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59071b7a5e67bf9b7e00c710ce5859c9",
+    "url": "/KoreanDynastyGenerator/index.html"
+  },
+  {
+    "revision": "2a8b57b5c75cd1c7ca42",
+    "url": "/KoreanDynastyGenerator/static/css/main.08981720.chunk.css"
+  },
+  {
+    "revision": "1f1256daabe698d33150",
+    "url": "/KoreanDynastyGenerator/static/js/2.d6a8b4e0.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/KoreanDynastyGenerator/static/js/2.d6a8b4e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a8b57b5c75cd1c7ca42",
+    "url": "/KoreanDynastyGenerator/static/js/main.d2055425.chunk.js"
+  },
+  {
+    "revision": "405e14badf281b56031c",
+    "url": "/KoreanDynastyGenerator/static/js/runtime-main.18a6e6cf.js"
+  }
+]);
